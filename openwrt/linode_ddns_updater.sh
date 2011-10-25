c40dd8c06a3bcf5f6a01688cf0a046ec033fea0a
Jeff Schornick <github@schornick.org>
2011-10-24 21:09:26 -0400

    Use a statefile to eliminate unecessary API calls - only query API if WAN IP is different than statefile (or missing) - record IP to statefile after query or update Additional comments and cleanup

--- openwrt/linode_ddns_updater.sh
+++ openwrt/linode_ddns_updater.sh
@@ -16,24 +16,56 @@ DOMAIN=$(uci get ddns.linode.domain)
 HOSTNAME=$(uci get ddns.linode.resource)
 API_KEY=$(uci get ddns.linode.api_key)
 
-# Openwrt doesn't have the root certs to verify the remote cert
+STATE_FILE=/var/state/linode_ddns.$HOSTNAME.$DOMAIN
+
+# Openwrt doesn't have the root certs to verify the remote cert,
+# so override the library's default WGET to skip the check
 WGET="wget --no-check-certificate "
-
-DOMAIN_ID=$(DomainFind $DOMAIN)
-RES_ID=$(DomainResFind $DOMAIN_ID $HOSTNAME)
-DNS_IP=$(DomainResGet $DOMAIN_ID $RES_ID TARGET)
-
-echo Linode DNS manager entry for $HOSTNAME.$DOMAIN: $DNS_IP
 
 IFACE=$(uci get network.wan.ifname)
 WAN_IP=$(ifconfig $IFACE | awk '/inet/ {print $2}' | sed 's/addr://')
 
+echo
 echo WAN interface IP: $WAN_IP
 
-if [ "$DNS_IP" = "$WAN_IP" ]; then
+if [ -f $STATE_FILE ]; then
+  LAST_IP=$(cat $STATE_FILE)
+  echo State file: $LAST_IP
+fi
+
+if [ "$LAST_IP" = "$WAN_IP" ]; then
+  echo "Skipping update"
+  echo
+  exit 0
+fi
+
+echo -n "Polling Linode DNS API."
+DOMAIN_ID=$(DomainFind $DOMAIN)
+echo -n "."
+RES_ID=$(DomainResFind $DOMAIN_ID $HOSTNAME)
+echo -n "."
+LINODE_IP=$(DomainResGet $DOMAIN_ID $RES_ID TARGET)
+echo " done."
+
+echo Linode DNS manager \($HOSTNAME.$DOMAIN\): $LINODE_IP
+
+if [ "$LINODE_IP" = "$WAN_IP" ]; then
   echo "Skipping update"
 else
-  DomainResSet $DOMAIN_ID $RES_ID TARGET $WAN_IP 
-  echo
+  echo -n "Performing update..."
+  response=$(DomainResSet $DOMAIN_ID $RES_ID TARGET $WAN_IP)
+  echo " done."
+  # Don't trust, spend an extra API call to get the actual value
+  # before saving to state file
+  LINODE_IP=$(DomainResGet $DOMAIN_ID $RES_ID TARGET)
+  echo Linode DNS manager entry is now: $LINODE_IP
 fi
+
+# even if we skipped the update due to a match, we may still
+# need to write a state file
+echo $LINODE_IP > $STATE_FILE
+
+echo
+
+exit 0
 
